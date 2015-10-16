ef7b0d6ae6c022cd7eb7ec2dcf9d1c44a99c7466
Yuta K <ugwis@icloud.com>
2015-10-16 10:26:04 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+﻿window.onload = function(){
+	var editor = ace.edit("editor");
+	editor.setValue("the new text here");
+    editor.setTheme("ace/theme/chaos");
+    editor.getSession().setMode("ace/mode/c_cpp");
+    editor.getSession().setUseSoftTabs(false);
+	editor.navigateLineEnd();
+	editor.setShowInvisibles(true);
+	editor.setShowPrintMargin(false);
+	editor.setOptions({
+		fontFamily:'Menlo,Monaco,Consolas',
+		fontSize: "20px"
+	});
+}
+
+editor.commands.addCommand({
+    name: 'Find',
+    bindKey: {win: 'Ctrl-F',  mac: 'Command-F'},
+    exec: function(editor) {
+        //...
+    },
+    readOnly: true // false if this command should not apply in readOnly mode
+});
