1355aeeffa9d97c46cbe1fd4e36a4229f79cbad7
Yuta K <orn_tk@me.com>
2018-05-03 23:03:38 +0900

    Change duration

--- script.js
+++ script.js
@@ -252,7 +252,7 @@ window.onload = function(){
 					document.getElementById("loading").style.opacity = "0";
 					setTimeout(function(){
 						document.getElementById("loading").style.display = "none";
-					},4000);
+					},2000);
 				});
 			}
 		}
